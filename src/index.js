40afad827750bc4101cb5e4c2e5070affceafad4
wunderbar <lankyauz@gmail.com>
2022-08-17 17:36:45 -0600

    - Created index.js function that clears the page of the previously generated elements. - Added an eventlistener to each header button that generates their respective page.

--- src/index.js
+++ src/index.js
@@ -2,7 +2,25 @@ import {homeLoad} from './homeLoad.js';
 import {menuLoad} from './menuLoad.js';
 import {contactLoad} from './contactLoad';
 
-// homeLoad();
-// menuLoad();
-contactLoad();
-console.log('its working!');
+const content = document.getElementById('content');
+const homeBtn = document.getElementById('homeBtn');
+const menuBtn = document.getElementById('menuBtn');
+const contactBtn = document.getElementById('contactBtn');
+
+homeBtn.addEventListener('click',function(){
+    clearPage();
+    homeLoad();
+});
+menuBtn.addEventListener('click',function(){
+    clearPage();
+    menuLoad();
+});
+contactBtn.addEventListener('click',function(){
+    clearPage();
+    contactLoad();
+});
+
+function clearPage(){
+    content.replaceChildren();
+};
+homeLoad();
